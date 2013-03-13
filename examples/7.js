0617f18e582f5f0f844781d34aa7538129f13e7f
Evan Oxfeld <eoxfeld@nearinfinity.com>
2013-03-13 13:01:00 -0400

    Tweak examples

--- examples/7.js
+++ examples/7.js
@@ -1,11 +1,12 @@
 var MatchStream = require('match-stream');
 
-//Pipe in "Hello World"
+//Stream chunks until a match is found, then end the stream
+//Ex. Pipe in "Hello World", pass along "Hello "
 
-var ms = new MatchStream({ pattern: 'World'}, function (buf, matched) {
-	if (!matched) {
-		return this.push(buf);
-	}
-	this.push(buf);
-	return this.push(null); //signal end of data
+var ms = new MatchStream({ pattern: 'World'}, function (data, matched) {
+  if (!matched) {
+    return this.push(data);
+  }
+  this.push(data);
+  return this.push(null); //signal end of data
 });
--- examples/8.js
+++ examples/8.js
@@ -1,17 +1,8 @@
-'use strict';
-
-module.exports = Match;
-
-var Transform = require('stream').Transform;
-var inherits = require("util").inherits;
-var Buffers = require('buffers');
+...
 
 inherits(Match, Transform);
-
 function Match(opts, matchFn) {
-
 	...
-
   Transform.call(this);
 }
 
@@ -21,6 +12,7 @@ Match.prototype._transform = function (chunk, encoding, callback) {
 
   var index = this._bufs.indexOf(pattern);
   if (index >= 0) {
+    //call matchFn for every match within a given chunk
     processMatches.call(this, index, pattern, callback);
   } else {
     this._matchFn(this._bufs.splice(0, this._bufs.length - chunk.length));
@@ -28,9 +20,4 @@ Match.prototype._transform = function (chunk, encoding, callback) {
   }
 };
 
-//call matchFn for every match within a given chunk
-function processMatches(index, pattern, callback) {
-  
-	...
-
-}
+...
